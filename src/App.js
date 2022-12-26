0bd3e38393cb829ab0d34318aed132173ee2a9a8
jafar <jafar.fahimi12@gmail.com>
2022-12-26 14:01:11 +0430

    imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header.js";
+import ProductDetail from "./components/ProductDetail.js";
+import ProductListing from "./components/ProductListing.js";
+import ProductComponent from "./components/ProductComponent.js";
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    // npm audit fix , npm i react-router-dom
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Header />
+          <ProductComponent />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

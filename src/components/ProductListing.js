5598cf87793dab94f2bb7f340585cbf4f1c04688
jafar <jafar.fahimi12@gmail.com>
2022-12-29 23:11:22 +0430

    productListing.js modified

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,7 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { useEffect } from "react";
+import {} from "../redux/actions/ProduceActions.js";
+
 const ProductListing = () => {
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products").catch((err) => {
+      console.log(err, err.message);
+    });
+    // dispatch(setProducts(response.data));
+    dispatch(setProducts([1,23,3]));
+    console.log(response.data);
+  };
+  console.log("products; 💕💕💕💕🏆🏆🏆🏆", products);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <h2>ProductListing</h2>
+      <h2>Product Listing File</h2>
     </div>
   );
 };

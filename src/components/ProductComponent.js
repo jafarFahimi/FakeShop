943738385fb4738ce4f35594bb68f78ea06db8bf
jafar <jafar.fahimi12@gmail.com>
2022-12-29 23:11:38 +0430

    ProductComponent.js modified

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 // an individual product
 const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  // const { id, title } = products[0];
   return (
     <div>
       <h2>ProductComponent</h2>

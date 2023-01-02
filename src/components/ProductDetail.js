30e583853953ef0ebbad305950270aff531d0a39
jafar <jafar.fahimi12@gmail.com>
2023-01-02 08:17:16 +0430

    ProductDetail.js added

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,9 +1,35 @@
+import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectedProducts } from "../redux/actions/ProduceActions";
+
 const ProductDetail = () => {
-    return (
-      <div>
-        <h2>ProductDetail</h2>
-      </div>
-    );
-  };
-  export default ProductDetail;
+  const param = useParams();
+  const product = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+  const { productId } = param;
   
+  const fetchProductDetail = () => {
+    axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .then((res) => {
+        dispatch(selectedProducts(res.data));
+      })
+      .catch((err) => alert(err));
+  };
+  // console.log(product); // check if state has been changed by our dispatch
+  // Object { productId: "1" }
+  useEffect(() => {
+    {
+      productId && productId != "" && fetchProductDetail();
+    }
+  }, [productId]);
+  return (
+    <div>
+      <h2>ProductDetail</h2>
+    </div>
+  );
+};
+export default React.memo(ProductDetail);

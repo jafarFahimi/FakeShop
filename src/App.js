0b7f6a8bf00f40925592d300b5012a55d9022d59
jafar <jafar.fahimi12@gmail.com>
2023-01-02 08:17:06 +0430

    ProductComponent.js added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import ProductDetail from "./components/ProductDetail.js";
@@ -6,16 +7,14 @@ import ProductListing from "./components/ProductListing.js";
 import ProductComponent from "./components/ProductComponent.js";
 function App() {
   return (
-    // npm audit fix , npm i react-router-dom
     <div className="App">
       <BrowserRouter>
-        <Header /> 
+        <Header />
         <Routes>
           <Route path="/" exact element={<ProductListing />} />
           <Route path="/product/:productId" exact element={<ProductDetail />} />
           <Route>404 Not Found!</Route>
         </Routes>
-        <ProductComponent />
       </BrowserRouter>
     </div>
   );
--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,13 +1,28 @@
 import { useSelector } from "react-redux";
-
-// an individual product
+import { Link } from "react-router-dom";
 const ProductComponent = () => {
-  const products = useSelector((state) => state.allProducts.products);
-  // const { id, title } = products[0];
+  const { products } = useSelector((state) => state.allProducts); // allProducts come from combineReducers in index.js
+  const renderList = products.map((product) => {
+    const { price, id, title, image, category, description, rating } = product;
+    return (
+      <Link key={id} to={`/product/${id}`} className="w-25">
+        <div className=" w-25 m-2">
+          <p>{title}</p>
+          <img src={image} className="w-25" alt={title} />
+          <p>$ {price}</p>
+          <h5>{category}</h5>
+        </div>
+      </Link>
+    );
+  });
+
   return (
-    <div>
-      <h2>ProductComponent</h2>
+    <div className="container-lg">
+      <main className="d-flex row flex-wrap justify-content-center bg-info align-item-center">
+        {renderList}
+      </main>
     </div>
   );
 };
+// export default connect(mapStatetoProps, mapDispatchToProps)(ProductComponent);
 export default ProductComponent;
